6dc88b76226b7b96661aafcfa55ee2d288da00bd
王明超 <50796589+wmc1992@users.noreply.github.com>
2023-05-31 15:07:17 +0800

    2023-05-31 15:07:16

--- inference_test/inference.py
+++ inference_test/inference.py
@@ -1,3 +1,4 @@
+import json
 import argparse
 
 import torch
@@ -8,14 +9,29 @@ parser.add_argument("--model_name_or_path", default=None, required=True, type=st
 args = parser.parse_args()
 
 tokenizer = AutoTokenizer.from_pretrained(args.model_name_or_path)
-model = AutoModelForCausalLM.from_pretrained(args.model_name_or_path)
+model = AutoModelForCausalLM.from_pretrained(args.model_name_or_path).cuda()
+model.eval()
 
-s = "将下述英文文本翻译为"
-inputs = tokenizer(s, return_tensors="pt")
-outputs = model(**inputs)
-outputs = torch.argmax(outputs["logits"], dim=-1)
+s_list = [
+    "德州市纪委立即成立",
+    "请将下述中文文本",
+    "请将下述中文",
+    "请将下述",
+    "请将",
+]
 
-print("输入文本：")
-print(s)
-print("输出文本：")
-print(tokenizer.decode(outputs[0]))
+for s in s_list:
+    ids = tokenizer.encode(s)
+    input_ids = torch.LongTensor([ids]).cuda()
+    out = model.generate(
+        input_ids=input_ids,
+        max_length=150,
+        do_sample=False,
+        temperature=0,
+    )
+    out_text = tokenizer.decode(out[0])
+
+    print("输入文本：")
+    print(s)
+    print("输出文本：")
+    print(json.dumps(out_text, ensure_ascii=False, indent=2))
